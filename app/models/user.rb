d96fffc8957558b0a96de89074fd0d520b0c6069
tinamariathomas <codertina@gmail.com>
2016-01-07 17:32:17 +0530

    Validity check on user's email id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   validates :name, presence: true,length: {maximum:52}
-  validates :email, presence: true
+  # XVALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\./[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w]+@[\w]+\.[\w]+\z/
+  validates :email, presence: true, format: {with:VALID_EMAIL_REGEX}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @valid_user = User.new(name:"Luna Lovegood", email: "abc@gmail.com")
-    @invalid_user_email_missing = User.new(name:"Luna Lovegood")
-    @invalid_user_name_missing = User.new(email:"abc@gmail.com")
+    @valid_user = User.new(name: "Luna Lovegood", email: "abc@gmail.com")
+    @invalid_user_email_missing = User.new(name: "Luna Lovegood")
+    @invalid_user_name_missing = User.new(email: "abc@gmail.com")
   end
 
   test "should be a valid user" do
@@ -24,6 +24,22 @@ class UserTest < ActiveSupport::TestCase
     @valid_user.name="xy"*50
     assert_not @valid_user.valid?
 
+  end
+
+  test "valid email addresses should be accepted" do
+    valid_addresses = %w[tina@gmail.com TINAABC@yahoo.com Tina_abc@aol.com]
+    valid_addresses.each do |address|
+      @valid_user.email = address
+      assert @valid_user.valid?
+    end
+  end
+
+  test "invalid email addresses should be not accepted" do
+    invalid_addresses = %w[kitty kitty@ kitty@cat kitty@cat. kitty.com @cat.com ]
+    invalid_addresses.each do |address|
+      @valid_user.email = address
+      assert_not @valid_user.valid?
+    end
   end
 
 end
