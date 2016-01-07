a57c06f73c6cdbb40bf85ebdf77662a0af718d7b
tinamariathomas <codertina@gmail.com>
2016-01-07 15:01:45 +0530

    Added integration test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", home_path
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+  end
+
+end
