63a0ca08565548d37b74c6642fff25f56f380f3f
tinamariathomas <codertina@gmail.com>
2016-01-08 12:20:54 +0530

    Password length constraint on user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @valid_user = User.new(name: "Luna Lovegood", email: "abc@gmail.com", password: "foobar", password_confirmation: "foobar" )
-    @invalid_user_email_missing = User.new(name: "Luna Lovegood", password: "foobar", password_confirmation: "foobar")
-    @invalid_user_name_missing = User.new(email: "abc@gmail.com", password: "foobar", password_confirmation: "foobar")
+    @valid_user = User.new(name: "Luna Lovegood", email: "abc@gmail.com", password: "foobarhello", password_confirmation: "foobarhello" )
+    @invalid_user_email_missing = User.new(name: "Luna Lovegood", password: "foobarhello", password_confirmation: "foobarhello")
+    @invalid_user_name_missing = User.new(email: "abc@gmail.com", password: "foobarhello", password_confirmation: "foobarhello")
   end
 
   test "should be a valid user" do
@@ -53,6 +53,12 @@ class UserTest < ActiveSupport::TestCase
     @valid_user.save
     @duplicate_user.email = @valid_user.email.upcase
     assert_not @duplicate_user.valid?
+
+  end
+
+  test "password should have at least 8 characters" do
+    @valid_user.password = @valid_user.password_confirmation = "hello"
+    assert_not @valid_user.valid?
 
   end
 
